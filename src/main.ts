6cf6f598da4ae4753e8cd98cbbb0bb9ddf50bcef
ellison <ellison.edsilva@senacsp.edu.br>
2025-02-27 22:29:48 -0300

    teste altera

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   )
 
   useContainer(app.select(AppModule),{fallbackOnErrors:true})
-  app.enableCors();
+ 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
